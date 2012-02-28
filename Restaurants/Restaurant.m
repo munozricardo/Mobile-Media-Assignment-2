d2f67d3f04d648fcef6b0a578ccfaabf63a7674e
Ricardo Munoz <munoz.ricardo@gmail.com>
2012-02-27 20:11:19 -0500

    one little refinement, still working good :)

--- Restaurants/Restaurant.m
+++ Restaurants/Restaurant.m
@@ -34,7 +34,7 @@
         address = @"25 St. Marks Pl., New York, NY 10003";
         phone = @"Phone: 212-254-6363";
         shortReview = @"This place has the best Japanese vibe ever. The pachinko machines were a nice touch. The food is great, the prices are fair, service gets iffy when the place is busy, but otherwise, it's awesome. The crowd's pretty young, the beer is cheap and I'd recommend ordering the 'takoyaki'. I can't find it anywhere else! So good!";
-        restAge = [NSString stringWithFormat:@"%i (%i years ago)", yearOpened, restaurantAge];
+        restAge = [NSString stringWithFormat:@"Est. %i (%i years ago)", yearOpened, restaurantAge];
     }
     return self;
 }
